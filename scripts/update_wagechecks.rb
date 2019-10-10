9614d01c769a61a5f9b0b51f6fe4239dae659cf2
Luigi Ray-Montanez <luigi@codeforamerica.org>
2019-10-10 19:00:29 -0400

    Add update_wagechecks

---
+++ scripts/update_wagechecks.rb
@@ -0,0 +1,19 @@
+require File.expand_path('../config/environment', File.dirname(__FILE__))
+require 'csv'
+
+csv = CSV.open(ENV['CSV_PATH'], headers: true)
+csv.each.with_index do |row, index|
+  if (contact = Contact.find_by(phone_number: PhoneNumber.format(row['PH_NUM_APP'])))
+    contact.response = row["VERIFICATION_RETURNED"]
+    contact.save!
+  end
+
+  if (contact = Contact.find_by(phone_number: PhoneNumber.format(row['PH_NUM_CASE'])))
+    contact.response = row["VERIFICATION_RETURNED"]
+    contact.save!
+  end
+
+  puts "\n\n==== ROW #{index} ====\n\n" if index.multiple_of?(1000)
+end
+
+puts Contact.where(list: "wagecheck").group(:response).count
