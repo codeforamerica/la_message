1cc311d831f75ed6b20b848066764544aa3d2798
Luigi Ray-Montanez <luigi@codeforamerica.org>
2019-10-10 19:00:15 -0400

    Update update scripts for final outcomes

--- scripts/update_apps.rb
+++ scripts/update_apps.rb
@@ -5,29 +5,29 @@ def save_contact(phone_number, row)
   return if PhoneNumber.format(phone_number).nil?
 
   if (contact = Contact.find_by(phone_number: PhoneNumber.format(phone_number)))
-    description = case row['DESCRIPTION']
-                  when "SSICompApp" then "Proof of SSI"
-                  when "Earned Income-Interface" then "Earned Income"
-                  when "Unsigned LAP Questionnaire" then "Signed LaCHIP Affordable Plan Questionnaire"
-                  when "Social Security Number Unverified" then "Proof of Social Security Number"
-                  when "CitizenshipInfo" then "Proof of Citizenship"
-                  when "Unearned Income-Interface" then "Unearned Income"
-                  when "US Citizenship" then "Proof of Citizenship"
-                  when "Social Security Number missing" then "Proof of Social Security Number"
-                  when "Bank Account" then "Bank Statements"
-                  when "Life Insurance" then "Proof of Life Insurance Policy"
-                  when "Burial Plot" then "Burial Contract"
-                  when "Access to State Health Coverage" then "Copies of Medical Insurance Cards"
-                  when "Incarceration Source" then "Incarceration Details"
-                  when "Pregnancy" then "Proof of Pregnancy"
-                  else
-                    row['DESCRIPTION']
-                  end
+    # description = case row['DESCRIPTION']
+    #               when "SSICompApp" then "Proof of SSI"
+    #               when "Earned Income-Interface" then "Earned Income"
+    #               when "Unsigned LAP Questionnaire" then "Signed LaCHIP Affordable Plan Questionnaire"
+    #               when "Social Security Number Unverified" then "Proof of Social Security Number"
+    #               when "CitizenshipInfo" then "Proof of Citizenship"
+    #               when "Unearned Income-Interface" then "Unearned Income"
+    #               when "US Citizenship" then "Proof of Citizenship"
+    #               when "Social Security Number missing" then "Proof of Social Security Number"
+    #               when "Bank Account" then "Bank Statements"
+    #               when "Life Insurance" then "Proof of Life Insurance Policy"
+    #               when "Burial Plot" then "Burial Contract"
+    #               when "Access to State Health Coverage" then "Copies of Medical Insurance Cards"
+    #               when "Incarceration Source" then "Incarceration Details"
+    #               when "Pregnancy" then "Proof of Pregnancy"
+    #               else
+    #                 row['DESCRIPTION']
+    #               end
+    #
+    # contact.enrollment_documents << description unless contact.enrollment_documents.include? description
 
-    contact.enrollment_documents << description unless contact.enrollment_documents.include? description
-
-    contact.response = row['NEED_RESPONSE'] == "Yes" ? "no" : "yes"
-    puts "#{contact.phone_number}: #{contact.enrollment_documents} #{contact.response}"
+    contact.response = row['VERIFICATION_PENDING'] == "Yes" ? "no" : "yes"
+    puts "#{contact.phone_number}: #{contact.response}"
     contact.save
   end
 end
--- scripts/update_renewals.rb
+++ scripts/update_renewals.rb
@@ -2,26 +2,15 @@ require File.expand_path('../config/environment', File.dirname(__FILE__))
 require 'csv'
 
 csv = CSV.open(ENV['CSV_PATH'], headers: true)
-no_count = 0
-yes_count = 0
 csv.each.with_index do |row, index|
   next if row['CELLPH_NUM'] == 'NULL'
 
   if (contact = Contact.find_by(phone_number: PhoneNumber.format(row['CELLPH_NUM'])))
-    puts row["RESPONSE_RECEIVED"]
-    if row["RESPONSE_RECEIVED"] == "No"
-
-      contact.response = "no"
-      no_count += 1
-    else
-      contact.response = "yes"
-      yes_count += 1
-    end
+    contact.response = row["RESPONSE_RECEIVED"]
     contact.save!
   end
 
   puts "\n\n==== ROW #{index} ====\n\n" if index.multiple_of?(1000)
 end
 
-puts "# of 'no': #{no_count}"
-puts "# of 'yes': #{yes_count}"
+puts Contact.where(list: "oct-renewals").group(:response).count
