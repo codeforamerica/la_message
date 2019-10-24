7751791bc5c09a0b68fa16f6d4ae5b0d05f5e9f9
Luigi Ray-Montanez <luigi@codeforamerica.org>
2019-10-24 18:43:25 -0400

    Update October applications

--- app/campaign_messages/enrollment_documents_message.rb
+++ app/campaign_messages/enrollment_documents_message.rb
@@ -1,6 +1,6 @@
 class EnrollmentDocumentsMessage < CampaignMessage
   def self.recipients
-    Contact.mobile.opted_in.where(list: "oct-apps").with_enrollment_documents.not_received_message(self)
+    Contact.mobile.opted_in.where(list: "oct-apps").with_enrollment_documents
   end
 
   def send_message
--- scripts/update_apps.rb
+++ scripts/update_apps.rb
@@ -5,8 +5,7 @@ def save_contact(phone_number, row)
   return if PhoneNumber.format(phone_number).nil?
 
   if (contact = Contact.find_by(phone_number: PhoneNumber.format(phone_number)))
-    contact.list = "aug-apps"
-    contact.response = row['VERIFICATION_PENDING'] == "Y" ? "no" : "yes"
+    contact.response = row['VERIFICATION_PENDING'] == "Yes" ? "no" : "yes"
     puts "#{contact.phone_number}: #{contact.response}"
     contact.save
   end
@@ -17,4 +16,4 @@ csv.each do |row|
   save_contact(row['CELLPH_NUM'], row)
 end
 
-puts Contact.where(list: "aug-apps").group(:response).count
+puts Contact.where(list: "oct-apps").group(:response).count
