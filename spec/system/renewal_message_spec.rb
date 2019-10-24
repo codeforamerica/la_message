20eb7b2465e14a3d366fd219c9b1d698a0d56b5b
Luigi Ray-Montanez <luigi@codeforamerica.org>
2019-10-24 19:12:18 -0400

    Fix failing spec

--- spec/system/renewal_message_spec.rb
+++ spec/system/renewal_message_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe "Renewal Message", type: :system do
       renewal_date: "2019/01/05",
       list: "nov-renewals",
       carrier_type: "mobile",
+      response: "No Packet Returned",
       segment: 1,
       opted_in: true
     )
@@ -22,7 +23,7 @@ RSpec.describe "Renewal Message", type: :system do
 
     expect(SmsService).to have_received(:send_message) do |message|
       expect(message.to_phone_number).to eq contact.phone_number
-      expect(message.body).to include "It's time to renew your household's Medicaid coverage."
+      expect(message.body).to include "You can renew online at"
       expect(message.body).to include "January 5"
       expect(message.message_type).to eq "RenewalMessage"
     end
