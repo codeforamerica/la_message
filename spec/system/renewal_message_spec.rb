516d6e8955354f2e7853d1f64648158d93276895
Luigi Ray-Montanez <luigi@codeforamerica.org>
2019-11-15 17:07:49 -0500

    Fix specs

--- spec/system/renewal_message_spec.rb
+++ spec/system/renewal_message_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "Renewal Message", type: :system do
       first_name: "Brian",
       phone_number: "5551231234",
       renewal_date: "2019/01/05",
-      list: "nov-renewals",
+      list: "dec-renewals",
       carrier_type: "mobile",
       response: "No Packet Returned",
       segment: 1,
@@ -24,7 +24,6 @@ RSpec.describe "Renewal Message", type: :system do
     expect(SmsService).to have_received(:send_message) do |message|
       expect(message.to_phone_number).to eq contact.phone_number
       expect(message.body).to include "You can renew online at"
-      expect(message.body).to include "January 5"
       expect(message.message_type).to eq "RenewalMessage"
     end
   end
